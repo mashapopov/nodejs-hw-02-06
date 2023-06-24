4fbf1762efbccd478c1625201b13d40c977516ec
Maria Popova <0979867251m@gmail.com>
2023-06-25 01:06:00 +0300

    finish

--- models/contact.js
+++ models/contact.js
@@ -1,8 +1,43 @@
 const { Schema, model } = require("mongoose");
-const contactSchema = new Schema({
-  name: String,
-  email: String,
-  phone: String,
+const handleMongooseError = require("../helpers/handleMongooseError");
+const Joi = require("joi");
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+contactSchema.post("save", handleMongooseError);
+
+const addSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
 });
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
+});
+
+const schemas = {
+  addSchema,
+  updateFavoriteSchema,
+};
 const Contact = model("contact", contactSchema);
-module.exports = Contact;
+module.exports = {
+  Contact,
+  schemas,
+};
