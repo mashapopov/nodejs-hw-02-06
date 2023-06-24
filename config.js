914ed82684bf607ee8588203a870def0999c93b4
Maria Popova <0979867251m@gmail.com>
2023-06-24 22:33:23 +0300

    delate config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-const DB_HOST =
-  "mongodb+srv://masha:123456789z@cluster0.wgniqyc.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-module.exports = {
-  DB_HOST,
-};
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,18 +1,18 @@
 const express = require("express");
 
 const router = express.Router();
-const validateBody = require("../../midllewares/validateBody");
-const schemas = require("../../schemas/contacts");
+// const validateBody = require("../../midllewares/validateBody");
+// const schemas = require("../../schemas/contacts");
 const ctrl = require("../../controllers/contacts");
 
 router.get("/", ctrl.getAll);
 
-router.get("/:contactId", ctrl.getById);
+// router.get("/:contactId", ctrl.getById);
 
-router.post("/", validateBody(schemas.addSchema), ctrl.add);
+// router.post("/", validateBody(schemas.addSchema), ctrl.add);
 
-router.delete("/:contactId", ctrl.delateById);
+// router.delete("/:contactId", ctrl.delateById);
 
-router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
+// router.put("/:contactId", validateBody(schemas.addSchema), ctrl.updateById);
 
 module.exports = router;
